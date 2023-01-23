6e3318d754ecc378efa9bc0724ae2fb46804c34c
Nilay <nilaykamat03@gmail.com>
2023-01-24 04:39:26 +0530

    built the sidebar and added custom images.

---
+++ src/components/sidebar/index.js
@@ -0,0 +1,49 @@
+import { Link, NavLink } from 'react-router-dom'
+import './index.scss'
+import LogoN from '../../assets/images/Nilay_N.png'
+import LogoSubtitle from '../../assets/images/subtitle.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons'
+
+const Sidebar = () => (
+    <div className='nav-bar'>
+        <Link className='logo' to='/ '>
+            <img src={LogoN} alt='logo' />
+            <img className='sub-logo' src={LogoSubtitle} alt='Nilay' />
+        </Link>
+        <nav>
+            <NavLink exact="true" activeclassname="active" to="/">
+                <FontAwesomeIcon icon={faHome} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                <FontAwesomeIcon icon={faUser} color='#4d4d4e'/>
+            </NavLink>
+            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                <FontAwesomeIcon icon={faEnvelope} color='#4d4d4e'/>
+            </NavLink>
+        </nav>
+
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/nilay-kamat-66b090225/'>
+                    <FontAwesomeIcon icon={faLinkedinIn} color="#4d4d4e"/>
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/Nilsiloid'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e"/>
+                </a>
+            </li>
+
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.instagram.com/kamatnilay/'>
+                    <FontAwesomeIcon icon={faInstagram} color="#4d4d4e"/>
+                </a>
+            </li>
+        </ul>
+    </div>
+)
+
+export default Sidebar
